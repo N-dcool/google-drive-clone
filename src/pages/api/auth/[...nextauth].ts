43befa3dab768abc8664653e7abaefb5698a9a64
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-09-15 21:51:04 +0530

    production-ready ✔️ version:1

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,17 +6,17 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider(<GithubAuth>{
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
+    GithubProvider(<GithubAuth>{
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     // DiscordProvider(<DiscordAuth>{
     //   clientId: process.env.DISCORD_CLIENT_ID,
     //   clientSecret: process.env.DISCORD_CLIENT_SECRET,
     // }),
     GoogleProvider(<GoogleAuth>{
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
